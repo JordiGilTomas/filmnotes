4b0a8a66857ee5f0d50ff500487d7386e16e5739
JordiGilTomas <gil.tomas.jordi@gmail.com>
2020-04-23 21:35:57 +0200

    Configured start script on package.json * Set url variable to use fetch

--- src/public/model/model.js
+++ src/public/model/model.js
@@ -1,2 +1,4 @@
+const url = window.location.href;
+
 const getMovies = async (movie) =>
-  await (await fetch(`http://localhost:3000/buscarPeliculas/${movie}`)).json();
+  await (await fetch(`${url}buscarPeliculas/${movie}`)).json();
